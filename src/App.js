2c1bdff6f91ba6d8d15d356f29ebb579beec2afc
Manoj C N <manojcn0302@gmail.com>
2023-10-04 21:34:59 +0530

    Fetch and Export

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FetchExcelData from './components/FetchExcelData/FetchExcelData';
+
+function App() {
+  return (
+    <div className="App">
+      <FetchExcelData />
+    </div>
+  );
+}
+
+export default App;
